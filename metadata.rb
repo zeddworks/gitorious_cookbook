0213cddf4b0f0ad4cee003470082f3b9ee5d317c
Scott McLeod <scott.mcleod@zeddworks.com>
2011-08-08 11:09:11 -0400

    Update email address

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 maintainer       "ZeddWorks"
-maintainer_email "smcleod@zeddworks.com"
+maintainer_email "scott.mcleod@zeddworks.com"
 license          "Apache 2.0"
 description      "Installs/Configures gitorious"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
